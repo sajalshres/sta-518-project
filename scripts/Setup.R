682f4538fe96620f73d7e0932b949f018e3955ab
sajal <sajal.shres@gmail.com>
2022-07-26 06:07:11 +0545

    added setup script

---
+++ scripts/Setup.R
@@ -0,0 +1,18 @@
+# Required packages
+packages <- c(
+  "tidyverse", 
+  "shiny",
+  "shinydashboard",
+  "plumber"
+)
+
+# Find installed packages
+is_packages_installed <- packages %in% rownames(installed.packages())
+
+# Install packages if missing
+if (any(is_packages_installed == FALSE)) {
+  install.packages(packages[!is_packages_installed], repos = "http://cran.us.r-project.org")
+}
+
+# Load packages
+invisible(lapply(packages, library, character.only = TRUE))
